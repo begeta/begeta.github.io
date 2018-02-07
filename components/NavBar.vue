fc55a5adc60cb076bb4df11d4aa30f709949fb9a
Chanwit Piromplad <kingkong2103@gmail.com>
2018-02-07 11:16:48 +0700

    re layout

---
+++ components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-55px ">
+    <a class="navbar-item is-full-width pd-l-15px pd-r-15px mg-vtc-10px">
+      <img class="is-brand-size" src="/images/logo-atomic.svg" alt="Atomic">
+    </a>
+  </div>
+</template>
