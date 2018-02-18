d43d8764e863c9c58d5d9c0c00c1be22d7b49a05
Chanwit Piromplad <kingkong2103@gmail.com>
2018-02-18 23:41:52 +0700

    add detail

--- pages/alignment/flexdirection.vue
+++ pages/alignment/flexdirection.vue
@@ -5,6 +5,14 @@
 
     <h3>Property</h3>
     <table-atomic :data="property"></table-atomic>
+
+    <h3>Value</h3>
+    <table-atomic :data="value"></table-atomic>
+
+    <h3>Responsive YES</h3>
+    <table-atomic :data="responsive"></table-atomic>
+
+    <h3>Hover NO</h3>
   </div>
 </template>
 
@@ -19,6 +27,14 @@ export default {
       property: [
         ['Class', 'f-drt-[value]-[responsive]'],
         ['Property', 'flex-direction']
+      ],
+      value: [
+        ['Class', 'row', 'column'],
+        ['Value Description', 'สำหรับ "row"', 'สำหรับ "column"']
+      ],
+      responsive: [
+        ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
+        ['Breakpoints', 'up to 768px', 'from 769px', 'from 1024px', 'from 1408px']
       ]
     }
   }
