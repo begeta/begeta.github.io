c41b358ea0702afc4438d51aa2203e05768be70f
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-02-19 11:15:13 +0700

    fix table component

--- components/TableAtomic.vue
+++ components/TableAtomic.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="dp-flex mg-5px">
     <div class="dp-flex f-drt-column" :key="keytr" v-for="(tr, keytr) in data">
-      <div class="bd-w-1px bd-st-solid bd-cl-grey pd-5px" :key="keytd" v-for="(td, keytd) in tr">
-        {{td}}
+      <div class="bd-w-1px bd-st-solid bd-cl-grey pd-5px h-45px" :key="keytd" v-for="(td, keytd) in tr">
+        <div v-if="isColorCode(td)">
+          <div :style="{ backgroundColor: td }" class="f-left w-30px h-30px bd-w-1px bd-st-solid bd-cl-light bd-rd-4px"></div>
+          <div class="f-left pd-t-5px pd-l-10px">{{ td }}</div>
+        </div>
+        <span v-else>{{ td }}</span>
       </div>
     </div>
   </div>
@@ -12,6 +16,15 @@
 export default {
   props: [
     'data'
-  ]
+  ],
+  methods: {
+    isColorCode (str) {
+      const pattern = new RegExp('^#(?:[0-9a-fA-F]{3}){1,2}$')
+      return pattern.test(str)
+    }
+  }
 }
 </script>
+
+<style scoped>
+</style>
---
+++ pages/background/backgroundcolor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="pd-10px">
+    <h1>Background Color</h1>
+    <div>ใช้กำหนดสีของ Background ให้กับ Element</div>
+
+    <h3>Property</h3>
+    <table-atomic :data="property"></table-atomic>
+
+    <h3>Value</h3>
+    <table-atomic :data="value"></table-atomic>
+
+    <h3>Responsive YES</h3>
+    <table-atomic :data="responsive"></table-atomic>
+
+    <h3>Hover NO</h3>
+  </div>
+</template>
+
+<script>
+import TableAtomic from '@/components/TableAtomic'
+export default {
+  components: {
+    TableAtomic
+  },
+  data () {
+    return {
+      property: [
+        ['Class', 'bg-cl'],
+        ['Property', 'background-color']
+      ],
+      value: [
+        ['Class', 'primary', 'white', 'light', 'black', 'link', 'info', 'success', 'warning', 'danger'],
+        ['Value Description', '#1066FD', '#FFFFFF', '#F5F5F5', '#000000', '#3675DA', '#2B9EEC', '#32D065', '#FEDD62', '#FC3C63']
+      ],
+      responsive: [
+        ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
+        ['Breakpoints', 'up to 768px', 'from 769px', 'from 1024px', 'from 1408px']
+      ]
+    }
+  }
+}
+</script>
+
