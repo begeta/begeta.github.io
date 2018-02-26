200ee15d2e85fe27cd3c58bd2d64d26e457db210
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-02-26 12:06:53 +0700

    add border radius

---
+++ pages/border/borderradius.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="pd-10px">
+    <h1>Border Radius</h1>
+    <div>ใช้กำหนดรัศมีของ Border</div>
+
+    <h3>Property</h3>
+    <table-atomic :data="property"></table-atomic>
+
+    <h3>Value</h3>
+    <table-atomic :data="value"></table-atomic>
+
+    <h3>Responsive YES</h3>
+    <table-atomic :data="responsive"></table-atomic>
+
+    <h3>Hover NO</h3>
+  </div>
+</template>
+
+<script>
+import TableAtomic from '@/components/TableAtomic'
+export default {
+  components: {
+    TableAtomic
+  },
+  data () {
+    return {
+      property: [
+        ['Class', 'bd-rd', 'bd-t-l-rd', 'bd-t-r-rd', 'bd-bt-l-rd', 'bd-bt-r-rd'],
+        ['Property', 'border-radius', 'border-top-left-radius', 'border-top-right-radius', 'border-bottom-left-radius', 'border-bottom-right-radius']
+      ],
+      value: [
+        ['Class', '0px', '1px', '2px', '3px', '4px', '5px', '10px', '15px', '20px', '25px', '30px'],
+        ['Value Description', '0px', '1px', '2px', '3px', '4px', '5px', '10px', '15px', '20px', '25px', '30px']
+      ],
+      responsive: [
+        ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
+        ['Breakpoints', 'up to 768px', 'from 769px', 'from 1024px', 'from 1408px']
+      ]
+    }
+  }
+}
+</script>
