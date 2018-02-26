9ed503788da61a1831c79a8c3a232141ad3a9009
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-02-26 12:03:09 +0700

    add border color

---
+++ pages/border/bordercolor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="pd-10px">
+    <h1>Border Color</h1>
+    <div>ใช้กำหนดสีของ Border</div>
+
+    <h3>Property</h3>
+    <table-atomic :data="property"></table-atomic>
+
+    <h3>Value</h3>
+    <table-atomic :data="value"></table-atomic>
+
+    <h3>Responsive YES</h3>
+    <table-atomic :data="responsive"></table-atomic>
+
+    <h3>Hover NO</h3>
+  </div>
+</template>
+
+<script>
+import TableAtomic from '@/components/TableAtomic'
+export default {
+  components: {
+    TableAtomic
+  },
+  data () {
+    return {
+      property: [
+        ['Class', 'bd-cl'],
+        ['Property', 'border-color']
+      ],
+      value: [
+        ['Class', 'primary', 'white', 'light', 'black', 'link', 'info', 'success', 'warning', 'danger'],
+        ['Value Description', '#1066FD', '#FFFFFF', '#F5F5F5', '#000000', '#3675DA', '#2B9EEC', '#32D065', '#FEDD62', '#FC3C63']
+      ],
+      responsive: [
+        ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
+        ['Breakpoints', 'up to 768px', 'from 769px', 'from 1024px', 'from 1408px']
+      ]
+    }
+  }
+}
+</script>
