2870b77c038e79aaee6906bf5b0877c3a4a9f18b
Chanwit Piromplad <kingkong2103@gmail.com>
2018-03-01 10:30:03 +0700

    add cursor

--- pages/boxsize.vue
+++ pages/boxsize.vue
@@ -12,7 +12,7 @@
     <h3>Property</h3>
     <table-atomic :data="property"></table-atomic>
 
-    <h3>Value (Top, Bottom, Left, Right)</h3>
+    <h3>Value</h3>
     <table-atomic :data="value"></table-atomic>
 
     <h3>Responsive YES</h3>
--- pages/cursor.vue
+++ pages/cursor.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pd-10px">
-    <h1>Color</h1>
-    <div>เป็น Atomic Class ที่ใช้จัดการ Style ที่เกี่ยวกับ Color ประกอบด้วย</div>
+    <h1>Cursor</h1>
+    <div>เป็น Atomic Class ที่ใช้จัดการ Style ที่เกี่ยวกับ Cursor ประกอบด้วย</div>
 
     <h3>Property</h3>
     <table-atomic :data="property"></table-atomic>
@@ -25,12 +25,12 @@ export default {
   data () {
     return {
       property: [
-        ['Class', 'cl'],
-        ['Property', 'color']
+        ['Class', 'cs'],
+        ['Property', 'cursor']
       ],
       value: [
-        ['Class', 'primary', 'white', 'light', 'black', 'link', 'info', 'success', 'warning', 'danger'],
-        ['Value Description', '#1066FD', '#FFFFFF', '#F5F5F5', '#000000', '#3675DA', '#2B9EEC', '#32D065', '#FEDD62', '#FC3C63']
+        ['Class', 'pointer', 'help', 'not-allowed'],
+        ['Value Description', 'สำหรับ "pointer"', 'สำหรับ "help"', 'สำหรับ "not-allowed"']
       ],
       responsive: [
         ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
