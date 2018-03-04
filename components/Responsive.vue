53775ddb3170191fdf913edd404b4bc0daaf5387
Chanwit Piromplad <kingkong2103@gmail.com>
2018-03-04 09:28:33 +0700

    add typo, fontsize and responsive table

---
+++ components/Responsive.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Responsive YES</h3>
+    <table-atomic :data="responsive"></table-atomic>
+  </div>
+</template>
+
+<script>
+import TableAtomic from './TableAtomic.vue'
+export default {
+  components: {
+    TableAtomic
+  },
+  data () {
+    return {
+      responsive: [
+        ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
+        ['Breakpoints', 'up to 768px', 'from 769px', 'from 1024px', 'from 1408px']
+      ]
+    }
+  }
+}
+</script>
