3cc20c12860118d9e026c3b91f2e2d6bcf0e6895
Chanwit Piromplad <kingkong2103@gmail.com>
2018-03-06 10:33:29 +0700

    add text-align

---
+++ pages/typography/textalign.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pd-10px">
+    <h1>Text Align</h1>
+    <div>ใช้สำหรับกำหนดความหนาของ Alignment ของ Inline Content ด้านใน Container</div>
+
+    <h3>Property</h3>
+    <table-atomic :data="property"></table-atomic>
+
+    <h3>Value</h3>
+    <table-atomic :data="value"></table-atomic>
+
+    <h3>Responsive YES</h3>
+    <responsive/>
+
+    <h3>Hover NO</h3>
+  </div>
+</template>
+
+<script>
+import TableAtomic from '@/components/TableAtomic'
+import Responsive from '@/components/Responsive'
+
+export default {
+  components: {
+    TableAtomic,
+    Responsive
+  },
+  data () {
+    return {
+      property: [
+        ['Class', 't-al'],
+        ['Property', 'text-align']
+      ],
+      value: [
+        ['Class', 'left', 'right', 'center', 'justify'],
+        ['Value Description', 'สำหรับ "left"', 'สำหรับ "right"', 'สำหรับ "center"', 'สำหรับ "justify"']
+      ],
+    }
+  }
+}
+</script>
