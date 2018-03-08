438a8739ff2850ae0f5711208cc9ecd70f99a21b
Chanwit Piromplad <kingkong2103@gmail.com>
2018-03-08 12:11:04 +0700

    add white space

---
+++ pages/typography/whitespace.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pd-10px">
+    <h1>White Space</h1>
+    <div>ใช้สำหรับกำหนดการจัดการกับ White Space</div>
+
+    <h3>Property</h3>
+    <table-atomic :data="property"></table-atomic>
+
+    <h3>Value</h3>
+    <table-atomic :data="value"></table-atomic>
+
+    <h3>Responsive YES</h3>
+    <responsive/>
+
+    <h3>Hover NO</h3>
+  </div>
+</template>
+
+<script>
+import TableAtomic from '@/components/TableAtomic'
+import Responsive from '@/components/Responsive'
+
+export default {
+  components: {
+    TableAtomic,
+    Responsive
+  },
+  data () {
+    return {
+      property: [
+        ['Class', 'w-sp'],
+        ['Property', 'white-space']
+      ],
+      value: [
+        ['Class', 'normal', 'no-wrap', 'pre', 'pre-line', 'pre-wrap'],
+        ['Value Description', 'สำหรับ "normal"', 'สำหรับ "no-wrap"', 'สำหรับ "pre"', 'สำหรับ "pre-line"', 'สำหรับ "pre-wrap"']
+      ],
+    }
+  }
+}
+</script>
