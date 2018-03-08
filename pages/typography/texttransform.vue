74fa40873bd4cc653bbe445a9ae686dcb0d18dba
Chanwit Piromplad <kingkong2103@gmail.com>
2018-03-08 12:10:54 +0700

    add text transform

---
+++ pages/typography/texttransform.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pd-10px">
+    <h1>Text Transform</h1>
+    <div>ใช้กำหนด Text Transform ให้กับ Text ของ Element</div>
+
+    <h3>Property</h3>
+    <table-atomic :data="property"></table-atomic>
+
+    <h3>Value</h3>
+    <table-atomic :data="value"></table-atomic>
+
+    <h3>Responsive YES</h3>
+    <responsive/>
+
+    <h3>Hover NO</h3>
+  </div>
+</template>
+
+<script>
+import TableAtomic from '@/components/TableAtomic'
+import Responsive from '@/components/Responsive'
+
+export default {
+  components: {
+    TableAtomic,
+    Responsive
+  },
+  data () {
+    return {
+      property: [
+        ['Class', 't-tf'],
+        ['Property', 'text-transform']
+      ],
+      value: [
+        ['Class', 'capitalize', 'lowercase', 'uppercase', 'none'],
+        ['Value Description', 'สำหรับ "capitalize"', 'สำหรับ "lowercase"', 'สำหรับ "uppercase"', 'สำหรับ "none"']
+      ],
+    }
+  }
+}
+</script>
