26dbc97bf51bfe0f6a8b464884b5e4b8429ff118
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-03-12 11:23:57 +0700

    add doc fit-content

--- pages/boxsize.vue
+++ pages/boxsize.vue
@@ -35,8 +35,8 @@ export default {
         ['Property', 'width', 'height', 'min-width', 'min-height', 'max-width', 'max-height']
       ],
       value: [
-        ['Class', 'auto', '0px 1px 2px 3px 4px 5px 6px 7px 8px 9px 10px 12px 14px 15px 20px 25px 30px 50px 60px 90px 80px 90px 100px', '0% 10% 20% 30% 40% 50% 60% 70% 80% 90% 100%'],
-        ['Value Description', 'ใช้สำหรับ "auto"', '0px 1px 2px 3px 4px 5px 6px 7px 8px 9px 10px 12px 14px 15px 20px 25px 30px 50px 60px 90px 80px 90px 100px', '0% 10% 20% 30% 40% 50% 60% 70% 80% 90% 100%']
+        ['Class', 'auto', 'fit-content', '0px 1px 2px 3px 4px 5px 6px 7px 8px 9px 10px 12px 14px 15px 20px 25px 30px 50px 60px 90px 80px 90px 100px', '0% 10% 20% 30% 40% 50% 60% 70% 80% 90% 100%'],
+        ['Value Description', 'ใช้สำหรับ "auto"', 'ใช้สำหรับ "fit-content"', '0px 1px 2px 3px 4px 5px 6px 7px 8px 9px 10px 12px 14px 15px 20px 25px 30px 50px 60px 90px 80px 90px 100px', '0% 10% 20% 30% 40% 50% 60% 70% 80% 90% 100%']
       ],
       responsive: [
         ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
