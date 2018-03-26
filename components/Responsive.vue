86911856274d2509e3810b2faecc79a927e0bd16
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-03-26 16:45:09 +0700

    add responsive class

--- components/Responsive.vue
+++ components/Responsive.vue
@@ -13,8 +13,8 @@ export default {
   data () {
     return {
       responsive_data: [
-        ['Class', 'mobile', 'tablet', 'desktop', 'widescreen'],
-        ['Breakpoints', 'up to 768px', 'from 769px', 'from 1024px', 'from 1408px']
+        ['Class', 'mobile', 'tablet', 'tablet-only', 'desktop', 'desktop-only', 'widescreen'],
+        ['Breakpoints', 'up to 768px', 'from 769px', '769px to 1023px', 'from 1024px', '1024px to 1215px', 'from 1216px']
       ]
     }
   }
