465341eae86ff3a3b3707d56d833ee524cb1f3ad
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-27 14:39:15 +0700

    fix width content

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,8 @@
 <div class="dp-flex f-drt-column">
   <div class="dp-flex f-drt-row f-drt-column-mobile">
     <nav-menu/>
-    <PerfectScrollbar class="h-100vh">
-      <div class="h-100vh w-100pct pd-l-20px">
-        <nuxt/>
-      </div>
+    <PerfectScrollbar class="h-100vh w-100pct pd-l-20px">
+      <nuxt/>
     </PerfectScrollbar>
   </div>
 </div>
